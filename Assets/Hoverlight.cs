b2152458ac0b052823d462d663f05b135fe36798
Bruno Vasconcelos <bruno.vasconcelos.dk@gmail.com>
2015-03-02 23:39:05 -0300

    PJ-Proto01

---
+++ Assets/Hoverlight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hoverlight : MonoBehaviour {
+
+	private Color startcolor;
+	void OnMouseEnter(){
+		Debug.Log ("hi");
+		startcolor = renderer.material.color;
+		renderer.material.color = Color.red;
+	}
+	void OnMouseExit()
+	{
+		renderer.material.color = startcolor;
+	}
+}
---
+++ Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	float moveSpeed = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (transform.position);
+
+		Vector2 pos = transform.position;
+		pos.x += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		transform.position = pos;
+
+	}
+}
